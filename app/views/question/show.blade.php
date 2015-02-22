6c25da59ebe8c9423d1ac2d88e713dfd49823744
Pramod <unknown.emlap@gmail.com>
2015-02-22 08:44:44 +0545

    display changed

--- app/views/question/show.blade.php
+++ app/views/question/show.blade.php
@@ -51,11 +51,11 @@ button.btn-success:hover{
 </div>
 <div class="col-md-10">
 	<p class="qn">{{$question->question}}</p>
+	<div class="row">
 		<div class="col-md-10">
 	
-<p class="like">{{'Likes :: '}}<span class="badge">{{$question->like}}</span>
-<?php echo "\t"; ?>	{{'Answers :: '}}<span class="badge">{{$question->like}}</span>
-{{ 'Discussions::'}}<span class="badge">{{$question->like}}</span></p>
+<p class="like">{{'Likes :: '}}{{$question->like}}
+</p>
 	</div>
 		<div class="col-md-1" >
 		{{ Form::open(array('url' => 'like')) }}
@@ -77,6 +77,8 @@ button.btn-success:hover{
 	</div>
 	
 </div>
+</div>
+</div>
 
 
 <?php 
@@ -97,12 +99,26 @@ $like = Like::where('user_id' ,'=' ,Auth::id())
 
 $discussions = Discussion::where('source_type' ,'=' ,'question')->where('source_id','=',$question->id)->orderBy('created_at')->get() ;
 
-
-foreach ($discussions as $discussion) {
-	echo User::find($discussion->user_id)->name.' :: '.$discussion->discussion.'<br>' ;
-}
 ?>
+
+@foreach ($discussions as $discussion) 
+	<div class="row">
+<div class="col-md-2">
 </div>
+
+<div class="col-md-2">
+	<span class="divider"></span>
+	<button type="button" class="btn btn-default btn-xs" aria-label="Left Align">
+	<span class="glyphicon glyphicon-user" aria-hidden="true"></span></button>
+ <p class="like">{{ User::find($discussion->user_id)->name}}</p>
+ </div>
+<div class="col-md-6">
+	<span class="divider"></span>
+ {{$discussion->discussion}}
+}
+</div>
+</div>
+@endforeach
 
 
 
@@ -119,7 +135,7 @@ foreach ($discussions as $discussion) {
 {{ Form::textarea('discussion' ,'' ,  array(
 'placeholder'   => 'Discuss here!' , 
 'class'         => 'form-control'   ,
-'rows'          => '2' ,
+'rows'          => '1' ,
 )) }}
 
 </div>
@@ -188,7 +204,7 @@ foreach ($discussions as $discussion) {
 	{{$answer->answer}}
 <br><br>
 <div class="col-md-10">
-<p class="like">Discussions::<span class="badge">{{$answer->like}}</span></p>
+
 </div>
 
 <div class="col-md-1">
@@ -261,7 +277,7 @@ $discussions = Discussion::where('source_type' ,'=' ,'answer')->where('source_id
 {{ Form::textarea('discussion' ,'' ,  array(
 'placeholder'   => 'Discuss here!' , 
 'class'         => 'form-control'   ,
-'rows'          => '2' ,
+'rows'          => '1' ,
 )) }}
 	</div>
 	<div class="col-md-2">
